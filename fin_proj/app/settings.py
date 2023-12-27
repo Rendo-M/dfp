458046a4e9a5d376f3f09d3bab25cc74e05e8758
Rendo-M <srvr@list.ru>
2023-12-27 03:40:54 +0300

    new one

--- fin_proj/app/settings.py
+++ fin_proj/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 185.125.200.156 ]
+ALLOWED_HOSTS = ['127.0.0.1', '185.125.200.156' ]
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
